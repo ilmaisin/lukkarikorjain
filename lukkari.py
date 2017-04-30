19fad40c209a5b7ee0ee24ff598448c20162bb42
Iiro Laiho <iiro.laiho@iki.fi>
2017-04-30 22:28:10 +0300

    Added filtering by text in summary

--- lukkari.py
+++ lukkari.py
@@ -2,8 +2,10 @@
 
 import cgi, cgitb, os
 #cgitb.enable(display=0, logdir="/home/protected/lukkari_log")
-cgitb.enable()
+cgitb.enable(format="plain")
 form = cgi.FieldStorage()
+
+os.write(1,b"Content-Type: text/plain\nCache-Control: max-age=21600\n\n") # CGI-otsakkeet
 
 from icalendar import Calendar, Event
 import urllib3, re, pytz
@@ -16,6 +18,9 @@ http = urllib3.PoolManager()
 r = http.request('GET', 'http://lukkari.turkuamk.fi/' + form.getvalue("campus") + '/vcal/' + form.getvalue("group") + ".ics")
 unicode_text = r.data.decode('latin1')
 cal = Calendar.from_ical(unicode_text)
+
+if form.getvalue("insummary"):
+    cal.subcomponents[:] = [comp for comp in cal.subcomponents if form.getvalue("insummary") in comp['SUMMARY']]
 
 if form.getvalue("tidy") == "yes":
     cal.subcomponents[:] = [comp for comp in cal.subcomponents if comp['LOCATION'] != ""]
@@ -30,5 +35,4 @@ for e in cal.walk('vevent'):
 
     e['SUMMARY'] = re.sub('^\[(.*?)\]\s', "", e['SUMMARY'])
 
-os.write(1,b"Content-Type: text/plain\nCache-Control: max-age=21600\n\n") # CGI-otsakkeet
 os.write(1,cal.to_ical())
